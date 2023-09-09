74aa8df9ed4f0fc8abb57cd7c9a82e7e92adcb04
Svetlana Ponomareva <svetlana.andreyevna@gmail.com>
2023-09-10 01:55:20 +0300

    l1

---
+++ Lecture_1/main.py
@@ -0,0 +1,110 @@
+# print(45343)
+# """       - to miss below code 
+# print(type(n))
+# print(type(n))
+# print(type(n))
+# print(type(n))
+# """
+
+
+# # print(45343)    ctr k -> ctr c
+# # print(45343)
+# print(45343)
+
+# a = 5
+# b = 5.89
+# c = 'hello'
+# print(a, b, c)
+# print(f'{a} - {b} - {c}')
+# print("{} - {} - {}".format(a,b,c))
+# print("Put 1st number: ")
+# a = int(input())
+# b = int(input("put 2nd numbder:"))
+
+# print(a, "+", b, "=", a+b)
+
+# c = 5.89
+# print(c)
+# print(type(c))
+# c = int(c)
+# print(c)
+# print(type(c))
+
+# a = 5.8956
+# b = 4.6546889000
+# print(round(a*b, 2))
+
+
+# username = input('put your name: ')
+# if username == 'masha':
+#     print("ura masha")
+# elif username == "misha":
+#     print(' hi misha')
+# elif username == "sveta":
+#     print('sveta vse mojet')
+# else:
+#     print('privet', username)
+
+# min delitel chisla
+
+# n = int (input())
+# flag = True
+# i = 2
+
+# while flag:
+#     if n % i == 0:
+#         flag = False
+#         print(i)
+#     elif i > n //2:
+#         print(n)
+#         flag = False
+#     i += 1
+
+# for i in range(15, 452, 15):
+#     print(i)
+
+# a = "qwerty"
+# print(a[0])
+
+# a = "qwerty"
+
+# for i in a:
+#     print(i)
+
+# line = ""
+# for i in range (5):
+#     line = ""
+#     for j in range (3):
+#         line += '*'
+#     print(line)
+
+# text = 'dAy day DAY YUE6TUE5YWfhdtjdd ghjfyuryjb rt'
+# print(len(text))
+# print('g' in text)
+# print(text.lower())
+# print(text.upper())
+
+# print(text.replace('day', 'trend'))
+
+# text = 'dAy day DAY YUE6TUE5YWfhdtjdd ghjfyuryjb rt'
+# print(text[0])
+# print(text[1])
+# print(text[-1])
+# print(text[len(text) - 1])
+
+# text = 'dAy day DAY YUE6TUE5YWfhdtjdd ghjfyuryjb rt'
+# # print(text[:])
+# # print(text[: 2]) #srez po 2 element  dA
+
+# # print(text[len(text) - 2:])  # srez s obratnoy storony rt
+# # print(text[20:])  # srez pervyh 20 elem, pechat poslednih
+
+# # print(text [2:9])
+# # print(text[3:-18])
+# # print(text[0: len(text): 6])  # ot 0 do konca stroki s shagom 6
+# # print(text[::6]) # ot 0 do konca stroki s shagom 6
+
+
+# text = 'dAy day DAY YUE6TUE5YWfhdtjdd ghjfyuryjb rt'
+# text = text[2:9] + text[-5] + text[:2]
+# print(text)
