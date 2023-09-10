59ac13d2384d59b731fa2f225b9c78ece62568e9
Svetlana Ponomareva <svetlana.andreyevna@gmail.com>
2023-09-10 09:59:04 +0300

    hm2

--- Seminar_3/HomeWork_3/task_3.py
+++ Seminar_3/HomeWork_3/task_3.py
@@ -25,6 +25,7 @@ russian = {1 : 'АВЕИНОРСТ', 2: 'ДКЛМПУ', 3: 'БГЁЬЯ', 4:'ЙЫ
 
 word = input('VVEDITE SLOVO CAPSLOCK-ом : ')
 
+
 if word.isascii() and word.isalpha():
     print('English text')
     print(sum([k for i in word for k, v in english.items() if i in v]))
@@ -36,3 +37,32 @@ else:
    
 
 
+eng = 'qwertyuiopasdfghjklzxcvbnm'
+
+rus = 'йцукенгшщзхъфывапролджэячсмитьбюё'
+
+d_English = {1:'AEIOULNSTR', 2:'DG', 3:'BCMP',
+                 4:'FHVWY', 5:"K" , 8:'JX', 10:'QZ'}
+d_Russian = {1:'АВЕИНОРСТ', 2:'ДКЛМПУ', 3:'БГЁЬЯ',
+                 4:'ЙЫ', 5:'ЖЗХЦЧ', 8:'ШЭЮ', 10:'ФЩЪ'}
+
+k = ('ящерица')
+
+if k[0].lower() in eng:
+    summa = 0
+    for letter in k:
+        for key, value in d_English.items():
+            if letter.upper() in value:
+                summa += key
+              
+
+
+else:
+    if k[0].lower() in rus:
+        summa = 0
+        for letter in k:
+            for key, value in d_Russian.items():
+                if letter.upper() in value:
+                    summa += key
+                    
+print(summa)
